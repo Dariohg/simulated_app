7c0674d7e9816649344478a797d83c42b16a42cb
Juliocpo946 <juliocesarp6273@gmail.com>
2025-12-05 13:38:48 -0600

    correcciones en la ui

--- packages/sentiment_analyzer/lib/src/presentation/notifications/widgets/video_player_modal.dart
+++ packages/sentiment_analyzer/lib/src/presentation/notifications/widgets/video_player_modal.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:chewie/chewie.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoPlayerModal extends StatefulWidget {
   final String videoUrl;
@@ -18,14 +17,9 @@ class VideoPlayerModal extends StatefulWidget {
 }
 
 class _VideoPlayerModalState extends State<VideoPlayerModal> {
-  VideoPlayerController? _videoPlayerController;
+  late VideoPlayerController _videoPlayerController;
   ChewieController? _chewieController;
-
-  YoutubePlayerController? _youtubeController;
-
-  bool _isYoutube = false;
-  bool _isLoading = true;
-  String? _error;
+  bool _isError = false;
 
   @override
   void initState() {
@@ -35,50 +29,41 @@ class _VideoPlayerModalState extends State<VideoPlayerModal> {
 
   Future<void> _initializePlayer() async {
     try {
-      final ytId = YoutubePlayer.convertUrlToId(widget.videoUrl);
-      if (ytId != null) {
-        _isYoutube = true;
-        _youtubeController = YoutubePlayerController(
-          initialVideoId: ytId,
-          flags: const YoutubePlayerFlags(
-            autoPlay: true,
-            mute: false,
-          ),
-        );
-      } else {
-        _isYoutube = false;
-        _videoPlayerController = VideoPlayerController.networkUrl(
-          Uri.parse(widget.videoUrl),
-        );
+      _videoPlayerController = VideoPlayerController.networkUrl(
+        Uri.parse(widget.videoUrl),
+      );
 
-        await _videoPlayerController!.initialize();
+      await _videoPlayerController.initialize();
 
-        _chewieController = ChewieController(
-          videoPlayerController: _videoPlayerController!,
-          autoPlay: true,
-          looping: false,
-          aspectRatio: _videoPlayerController!.value.aspectRatio,
-          errorBuilder: (context, errorMessage) {
-            return Center(
-              child: Text(errorMessage, style: const TextStyle(color: Colors.white)),
-            );
-          },
-        );
+      _chewieController = ChewieController(
+        videoPlayerController: _videoPlayerController,
+        autoPlay: true,
+        looping: false,
+        aspectRatio: _videoPlayerController.value.aspectRatio,
+        errorBuilder: (context, errorMessage) {
+          return Center(
+            child: Text(
+              errorMessage,
+              style: const TextStyle(color: Colors.white),
+            ),
+          );
+        },
+      );
+
+      if (mounted) {
+        setState(() {});
       }
     } catch (e) {
-      _error = e.toString();
-    } finally {
       if (mounted) {
-        setState(() => _isLoading = false);
+        setState(() => _isError = true);
       }
     }
   }
 
   @override
   void dispose() {
-    _videoPlayerController?.dispose();
+    _videoPlayerController.dispose();
     _chewieController?.dispose();
-    _youtubeController?.dispose();
     super.dispose();
   }
 
@@ -86,58 +71,27 @@ class _VideoPlayerModalState extends State<VideoPlayerModal> {
   Widget build(BuildContext context) {
     return Dialog(
       backgroundColor: Colors.black,
-      insetPadding: const EdgeInsets.all(10),
+      insetPadding: const EdgeInsets.all(16),
       child: Stack(
         alignment: Alignment.topRight,
         children: [
           Container(
             width: double.infinity,
-            height: 400,
+            height: 300, // Altura del reproductor
             alignment: Alignment.center,
-            child: _buildPlayerContent(),
+            child: _isError
+                ? const Text("Error al cargar video", style: TextStyle(color: Colors.white))
+                : _chewieController != null &&
+                _chewieController!.videoPlayerController.value.isInitialized
+                ? Chewie(controller: _chewieController!)
+                : const CircularProgressIndicator(),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CircleAvatar(
-              backgroundColor: Colors.black54,
-              child: IconButton(
-                icon: const Icon(Icons.close, color: Colors.white),
-                onPressed: widget.onClose,
-              ),
-            ),
+          IconButton(
+            icon: const Icon(Icons.close, color: Colors.white),
+            onPressed: widget.onClose,
           ),
         ],
       ),
     );
-  }
-
-  Widget _buildPlayerContent() {
-    if (_isLoading) {
-      return const CircularProgressIndicator(color: Colors.white);
-    }
-
-    if (_error != null) {
-      return Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text(
-          "Error al cargar video: $_error\nURL: ${widget.videoUrl}",
-          style: const TextStyle(color: Colors.white),
-          textAlign: TextAlign.center,
-        ),
-      );
-    }
-
-    if (_isYoutube && _youtubeController != null) {
-      return YoutubePlayer(
-        controller: _youtubeController!,
-        showVideoProgressIndicator: true,
-      );
-    }
-
-    if (!_isYoutube && _chewieController != null) {
-      return Chewie(controller: _chewieController!);
-    }
-
-    return const Text("No se pudo cargar el reproductor", style: TextStyle(color: Colors.white));
   }
 }
